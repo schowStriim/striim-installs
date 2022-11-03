2e736e111d05eff0700cb4356ad8f31f72b9083d
root <root@ip-172-31-9-4.us-west-2.compute.internal>
2022-11-03 15:05:02 +0000

    remove shell scripts

--- install/striim-redhat-uninstall.sh
+++
@@ -1,8 +0,0 @@
-# Uninstall Java (1.8)
-rm -rf /usr/lib/jvm/
-rm -rf /usr/bin/java
-
-# Uninstall Striim
-sudo rpm -e striim-node
-sudo rpm -e striim-dbms
-rm -rf /opt/striim/
--- install/striim-ubuntu-uninstall.sh
+++
@@ -1,8 +0,0 @@
-# Uninstall Java (1.8)
-rm -rf /usr/lib/jvm/
-rm -rf /usr/bin/java
-
-# Uninstall Striim
-sudo dpkg -r striim-node
-sudo dpkg -r striim-dbms
-rm -rf /opt/striim/
