07a2d8717f9d64edabf0f4a0f418c4c31c8cd917
EC2 Default User <ec2-user@ip-172-31-5-43.us-west-2.compute.internal>
2023-05-15 12:55:06 +0000

    adding the latest updates of striim_error_log_finder shell script

--- log_finder/striim_error_log_finder.sh
+++ log_finder/striim_error_log_finder.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
-###########################################################################################
-# Script name: striim_error_log_finder.sh                                                 #
-# Description: This shell script will extract ALL errors from the striim.server.log file. #
-# Usage: sudo ./striim_error_log_finder.sh /opt/striim/logs                               #
-###########################################################################################
+################################################################################################
+# Script name: striim_error_log_finder.sh                                                      #
+# Description: This shell script will extract ALL errors from the striim.server.log file OR    #
+#              from a specific application component.                                          #
+# Usage: sudo ./striim_error_log_finder.sh /opt/striim/logs/striim.server.log <component_name> # 
+################################################################################################
 
 GREEN=$'\e[0;32m'
 BLUE=$'\e[0;34m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
 
-LOG_DIR=$1
+LOG_FILE=$1
+APP_COMPONENT_NAME=$2
 
-if [[ -z "$LOG_DIR" ]] ;
+if [[ -z "$LOG_FILE" ]] ;
 then
     
-    printf "\nWARNING: Missing the log directory path. \n"
-    printf "Using defaulted log directory path: /opt/striim/logs \n"
-    printf "If the log file is in a different directory, please enter the log directory file path in the first argument. \n"
-    printf "For example: ./striim_error_log_finder.sh /opt/custom/path/dir/logs \n"
-    LOG_DIR=/opt/striim/logs
+    printf "\n${RED}ERROR: Missing the log directory path. Please enter the directory path of the striim.server.logs${NC} \n"
+    printf "${RED}For example: ./striim_error_log_finder.sh /opt/custom/path/dir/logs/ ${NC}\n"
+    exit 1
 fi
-
-LOG_FILE=$LOG_DIR/striim.server.log
 
 if [[ -f "$LOG_FILE" ]]; then
     
     TIMESTAMP=$(date +%s)
-    ERROR_LOG_FILE=$LOG_DIR/striim_error_logs_"$TIMESTAMP".log
-
-    grep -e "-ERROR" $LOG_FILE | awk '{print $0,"\n"}' > $ERROR_LOG_FILE
-    printf "Successfully parsed and generated the Striim error log file: $ERROR_LOG_FILE \n "
+    
+    if [[ ! -z "$APP_COMPONENT_NAME" ]] ; then
+        ERROR_LOG_FILE="$APP_COMPONENT_NAME"_striim_error_logs_"$TIMESTAMP".log
+        grep -e "-ERROR" $LOG_FILE | grep -e $APP_COMPONENT_NAME | awk '{print $0,"\n"}' > $ERROR_LOG_FILE
+        printf "${GREEN}Successfully parsed and generated the $APP_COMPONENT_NAME Striim error log file: $ERROR_LOG_FILE ${NC}\n "
+    else
+        ERROR_LOG_FILE=striim_error_logs_"$TIMESTAMP".log
+        printf "\n${BLUE}This execution will parse and generate all the ERROR messages from the striim.server.log ${NC}\n"
+        printf "${BLUE}If you want to print error logs of a specific application, enter the app name in the second argument when executing the script. ${NC}\n"
+        printf "${BLUE}For example: ./striim_error_log_finder.sh /opt/striim/custom/path admin.APP_COMPONENT_NAME ${NC}\n\n"
+        grep -e "-ERROR" $LOG_FILE | awk '{print $0,"\n"}' > $ERROR_LOG_FILE
+        printf "${GREEN}Successfully parsed and generated the Striim error log file: $ERROR_LOG_FILE ${NC}\n "
+    fi
+    
 else
-    printf "\n The striim.server.log file doesn't exist in the specified directory: $LOG_FILE \n"
+    printf "\n ${RED}The striim.server.log file doesn't exist in the specified directory: $LOG_FILE ${NC}\n"
     exit 1
 fi
